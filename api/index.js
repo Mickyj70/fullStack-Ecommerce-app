1cd553ac74cf172dfc5b48e0bcba11d2a642ff58
Mickyj77 <emrex1291@gmail.com>
2024-02-07 03:22:06 +0100

    database connection successful

--- api/index.js
+++ api/index.js
@@ -4,9 +4,13 @@ const app = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
+const connectDB = require("./config/DBconnection");
 
 //!connected port
 const port = process.env.PORT || 3500;
+
+//!connect to mongodb
+connectDB();
 
 //*middleware
 app.use(credentials);
@@ -19,11 +23,16 @@ app.use(express.urlencoded({ extended: false }));
 // built-in middleware for json
 app.use(express.json());
 
+//middleware for cookies
+app.use(cookieParser());
+
 //?routes
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+mongoose.connection.once("open", () => {
+  console.log("DB connected");
+
+  app.listen(PORT, () => console.log(`Server running on port ${port}`));
 });
