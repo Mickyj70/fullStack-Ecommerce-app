f844c80e51da76a764f3bbabf8856ba1e9feba0c
Mickyj77 <emrex1291@gmail.com>
2024-02-10 18:01:47 +0100

    setting up routes

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const credentials = require("./middleware/credentials");
 const cookieParser = require("cookie-parser");
 const mongoose = require("mongoose");
 const connectDB = require("./config/DBconnection");
@@ -13,7 +14,10 @@ const port = process.env.PORT || 3500;
 //!connect to mongodb
 connectDB();
 
-// Cross Origin Resource Sharing
+//? Handle options credentials check - before CORS!
+// ?and fetch cookies credentials requirement
+app.use(credentials);
+//? Cross Origin Resource Sharing
 app.use(cors(corsOptions));
 
 // built-in middleware to handle urlencoded form data
@@ -25,10 +29,10 @@ app.use(express.json());
 //middleware for cookies
 app.use(cookieParser());
 
-//?routes
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+//*routes
+// app.use("/api/register", require("./routes/register"));
+// app.use("/api/login", require("./routes/auth"));
+// app.use("/api/users", require("./routes/user"));
 
 mongoose.connection.once("open", () => {
   console.log("DB connected");
